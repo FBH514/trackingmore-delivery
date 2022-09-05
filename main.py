53d6c36c19ffd05adabc76a51fe447f992ba87bd
FBH514 <kittens-paring0f@icloud.com>
2022-09-05 11:15:49 -0400

    Removed bloat.

--- main.py
+++ main.py
@@ -15,24 +15,27 @@ class Delivery:
             "Tracking-Api-Key": self.api_key
         }
 
-    def save_tracking_data(self):
+    def save_tracking_data(self) -> None:
         url = self.base_url + "/" + self.api_version + "/trackings/get"
         results = requests.get(url, headers=self.headers)
         with open("results.json", "w") as f:
             json.dump(results.json(), f)
-        return results.json()
 
     @staticmethod
-    def get_tracking_data():
+    def get_tracking_data() -> dict:
         with open("results.json", "r") as f:
             return json.load(f)
 
-    def delivery_status(self):
+    def delivery_status(self) -> list:
         delivery = []
+        if not self.get_tracking_data()["code"] == 200:
+            return [self.get_tracking_data()["code"], self.get_tracking_data()["message"]]
         for key in self.get_tracking_data().keys():
             if key == "data":
                 data = self.get_tracking_data()[key][0]
                 for item in data.keys():
+                    if len(delivery) == 3:
+                        break
                     if item == "tracking_number":
                         delivery.append(data[item])
                     if item == "delivery_status":
@@ -43,7 +46,7 @@ class Delivery:
         delivery[2] = time[0]
         return delivery
 
-    async def main(self):
+    async def main(self) -> None:
         while True:
             self.save_tracking_data()
             data = self.delivery_status()
@@ -51,7 +54,7 @@ class Delivery:
                 f"Time – {datetime.now().strftime('%I:%M:%S %p')}"
                 f"\nTracking Number: {data[0]}"
                 f"\nDelivery Status: {data[1].upper()}"
-                f"\nUpdate Date: {data[2]}"
+                f"\nLast Update: {data[2]}"
             )
             print()
             await asyncio.sleep(60)
